1530d03e52b3296917d22e941307b8496f47f4e2
havszab <havaldaszab@gmail.com>
2019-01-15 10:36:04 +0100

    cart item's base created

---
+++ src/containers/Cart/Items/Item/Item.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const item = (props) => (
+    <div>
+        <div>{props.name}</div>
+        <div>{props.price}</div>
+        <div><button onClick={props.click}>Remove</button></div>
+    </div>
+);
+
+export default item;
