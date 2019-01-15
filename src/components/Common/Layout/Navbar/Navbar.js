e907509980b9878f826a3dc1be53e48b9ae04aa1
MattHowdy <mate.hodi9@gmail.com>
2019-01-15 11:25:38 +0100

    navbar vol1

--- src/components/Common/Layout/Navbar/Navbar.js
+++ src/components/Common/Layout/Navbar/Navbar.js
@@ -1,10 +1,19 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
+
+import Logo from '../Logo/Logo';
+import classes from './Navbar.css'
+
 
 const navbar = () => (
-        <div>
-            <div>LOGO</div>
-            <div>CARTICON</div>
-            <div>LOGINICON</div>
+        <div className={classes.Navbar}>
+            <ul>
+                <li><Link to={'/'}/><Logo/></li>
+                <li><Link to={'/sell-item'}>SELL</Link></li>
+                <li><Link to={'/cart'}>CART</Link></li>
+                <li><Link to={'/login'}>LOGIN</Link></li>
+                <li><Link to={'/login'}>LOGOUT</Link></li>
+            </ul>
         </div>
     );
 
