1adf70931decb140d0e835822975fd7b55c2aae4
havszab <havaldaszab@gmail.com>
2019-01-15 10:45:35 +0100

    cart implemented without backend and css

--- src/containers/Cart/Cart.js
+++ src/containers/Cart/Cart.js
@@ -1,11 +1,32 @@
 import React, {Component} from 'react';
+import Items from "./Items/Items";
 
 class Cart extends Component {
 
+    state = {
+        items: [
+            {
+                id: 1,
+                name: "some item",
+                price: 9.99
+            }
+        ]
+    };
+
+    getTotalPriceHandler = () => {
+        let totalPrice = 0;
+        this.state.items.map(item => {
+            totalPrice += item.price;
+        })
+        return totalPrice;
+    }
+
     render() {
+
         return (
             <div>
-                CART
+                <Items items={this.state.items}/>
+                <p>TOTAL: {this.getTotalPriceHandler}</p>
             </div>
         );
     }
--- src/containers/Cart/Items/Item/Item.js
+++ src/containers/Cart/Items/Item/Item.js
@@ -3,7 +3,7 @@ import React from "react";
 const item = (props) => (
     <div>
         <div>{props.name}</div>
-        <div>{props.price}</div>
+        <div>{props.price}$</div>
         <div><button onClick={props.click}>Remove</button></div>
     </div>
 );
