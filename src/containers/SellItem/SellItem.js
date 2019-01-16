c3da45ac7a6e9145ecfad58564396f75b37a340b
Mate Toth-Csak <tothcsakmate@gmail.com>
2019-01-16 11:00:02 +0100

    Selling items form created

--- src/containers/SellItem/SellItem.js
+++ src/containers/SellItem/SellItem.js
@@ -1,10 +1,20 @@
 import React, {Component} from 'react';
+import './SellItem.css';
+
 
 class SellItem extends Component {
     render() {
         return (
             <div>
-              SELL ITEM
+                <h1>Post an item to sell</h1>
+                <form>
+                    <p>Item details</p>
+                    <input type="text" name="item_name" id="item_name" placeholder="Item name"></input>
+                    <input type="text" name="item_price" id="item_price" placeholder="Price"></input>
+                    <input type="text" name="item_description" id="item_description" placeholder="Description"></input>
+                    <input type="text" name="item_picture" id="item_picture" placeholder="Picture URL"></input>
+                    <button type="submit">Submit</button>
+                </form>
             </div>
         );
     }
