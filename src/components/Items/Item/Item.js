62b6d554bd26412720566d5f93bfc189a8f85052
havszab <havaldaszab@gmail.com>
2019-01-17 10:00:22 +0100

    basic cart styling

--- src/components/Items/Item/Item.js
+++ src/components/Items/Item/Item.js
@@ -5,8 +5,8 @@ const item = (props) => (
     <div className={classes.Item}>
         <div><img src={props.src} style={{height: "25px", width: "25px"}} alt="" /></div>
         <div>{props.name}</div>
-        <div>{props.price}$</div>
-        <div><button onClick={props.click}>Remove</button></div>
+        <div className={classes.Price}>{props.price}$</div>
+        <button onClick={props.click} className={classes["Button-warning"]}>X</button>
     </div>
 );
 
--- src/containers/Cart/Cart.js
+++ src/containers/Cart/Cart.js
@@ -4,32 +4,63 @@ import axios from "axios";
 import {Link} from 'react-router-dom';
 
 
-const ZUULip = "192.168.160.182";
-const removeUrlBase = "http://" + ZUULip + ":8762/cart-service/removeItem?buyerId=";
+const ZUULserviceURL = "https://shitwish-zuul.azurewebsites.net";
+const removeUrlBase = "/cart-service/removeItem?buyerId=";
 const itemUrlPart = "&itemId=";
-//const getItemsUrl = "http://" + ZUULip + ":8762/cart-service/cartitems?buyerId=";
-const simpleItemUrl = "http://" + ZUULip + ":8762/item-service/items";
+//const getItemsUrl = "/cart-service/cartitems?buyerId=";
+const simpleItemUrl = "/item-service/items";
 
 class Cart extends Component {
 
     state = {
         totalPrice: 0,
-        items: [{
-            available: true,
-            description: "A stick",
-            id: 1,
-            img: "https://vignette.wikia.nocookie.net/tokipona/images/a/aa/Stick.png/revision/latest?cb=20171120043817",
-            name: "Stick",
-            price: 100
-        },
+        items: [
             {
-                id: 3,
-                name: "Trash",
-                description: "Everywhere",
-                img: "https://vignette.wikia.nocookie.net/tokipona/images/a/aa/Stick.png/revision/latest?cb=20171120043817",
-                price: 1300,
-                available: true
-            }]
+                "id": 1,
+                "name": "Stick",
+                "price": 100,
+                "available": true,
+                "img": "https://vignette.wikia.nocookie.net/tokipona/images/a/aa/Stick.png/revision/latest?cb=20171120043817",
+                "uploadDate": "2019-01-15",
+                "sellerId": 1
+            },
+            {
+                "id": 2,
+                "name": "Shit",
+                "price": 1500,
+                "available": true,
+                "img": "https://http2.mlstatic.com/almofada-emoticon-emoji-cocozinho-pou-grande-40x40-whatsapp-D_NQ_NP_635411-MLB20557478677_012016-F.jpg",
+                "uploadDate": "2019-01-15",
+                "sellerId": 2
+            },
+            {
+                "id": 3,
+                "name": "Trash",
+                "price": 1300,
+                "available": true,
+                "img": "https://toppng.com/public/uploads/preview/trash-can-11530995314kgh8pawz8u.png",
+                "uploadDate": "2019-01-15",
+                "sellerId": 1
+            },
+            {
+                "id": 4,
+                "name": "Wonder Woman",
+                "price": 10001,
+                "available": true,
+                "img": "https://www.clipartmax.com/png/middle/195-1951557_wonder-woman-png-edit-justice-league-by-bp251-wonder-woman-gal-gadot.png",
+                "uploadDate": "2019-01-15",
+                "sellerId": 1
+            },
+            {
+                "id": 5,
+                "name": "Little boy",
+                "price": 99,
+                "available": true,
+                "img": "https://clipart.info/images/ccovers/1496252513Justin-Bieber-PNG-Picture-2017.png",
+                "uploadDate": "2019-01-15",
+                "sellerId": 2
+            }
+        ]
     };
 
     componentDidMount() {
